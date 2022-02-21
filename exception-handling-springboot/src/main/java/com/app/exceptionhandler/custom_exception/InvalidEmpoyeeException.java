2e600107bb652b13c314bdd60f6689f7cf35166f
broshan989 <bhogeroshan98@gmail.com>
2022-02-21 14:48:40 +0530

    Revert "Exception handling in spring boot"
    This reverts commit aba30e11437fb4f632cce56e18ec7c2c6210a504.

--- exception-handling-springboot/src/main/java/com/app/exceptionhandler/custom_exception/InvalidEmpoyeeException.java
+++
@@ -1,9 +0,0 @@
-package com.app.exceptionhandler.custom_exception;
-
-@SuppressWarnings("serial")
-public class InvalidEmpoyeeException extends RuntimeException {
-	
-	public InvalidEmpoyeeException(String msg) {
-		super(msg);
-	}
-}
